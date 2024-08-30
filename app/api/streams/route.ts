ea2ae6a805c1f26d1119af843f5b5723ea646b03
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-08-30 16:24:57 +0530

    add: create stream end point

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import { YOUTUBE_REGEX } from "@/app/constants";  
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
@@ -9,10 +10,26 @@ const CreateStreamSchema = z.object({
 
 export async function POST(req: NextRequest) {
   try {
-    const data = CreateStreamSchema.parse(await req.json());
-    prismaClient.stream.create({
-      userId: data.creatorId,
-      
+    const data = CreateStreamSchema.parse(await req.json()); // convert req to json than check schema.
+    const isYouTube = YOUTUBE_REGEX.test(data.url); // check if url is a youtube url.
+
+    // Error handeling if url is not of youtube.
+    if (!isYouTube) {
+      return NextResponse.json(
+        { message: "Wrong URL format." },
+        { status: 411 }
+      );
+    }
+
+    const extractedId = data.url.split("?v=")[1]; // extracting video id from the youtube url.
+
+    await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type: "YouTube",
+      },
     });
   } catch (error) {
     return NextResponse.json(
---
+++ app/constants.ts
@@ -0,0 +1,1 @@
+export const YOUTUBE_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}$");
